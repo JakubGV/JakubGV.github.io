a72f11844a9225d907b323f23594517b73d0d4e8
jyi-jakubgvogel <kubi.vogel7@hotmail.com>
2022-01-13 17:46:31 -0500

    Change font, style header, add images, and create page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import WebFont from 'webfontloader'
+
 import './App.css';
+import headshot from './cropped_headshot_small.jpg'
+
+WebFont.load({
+  google: {
+    families: ['Open Sans']
+  }
+})
 
 function App() {
   const sections = ["Home", "About", "Resume", "Contact"]
   
   return (
     <div className="main">
+      
       <div className="menu-bar">
         <div className="name-left">
-          <h1>left</h1>
+          <h1 className="name">Jakub Vogel</h1>
         </div>
         <div className="row-right">
           {
@@ -23,9 +33,27 @@ function App() {
         </div>
       </div>
 
-      <div className="body">
-        <h1>body</h1>
+      <div className="pages">
+        <div className="home">
+          <div className="welcome-info">
+            <img src={headshot} alt="Headshot"/>
+            <div>Text under name here</div>
+          </div>
+        </div>
+
+        <div className="about">
+          about
+        </div>
+
+        <div className="resume">
+          resume
+        </div>
+
+        <div className="contact">
+          contact
+        </div>
       </div>
+    
     </div>
   );
 }
