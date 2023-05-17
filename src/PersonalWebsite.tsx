1e586e7f31d62436c7ff1da2f117fe272c9d5116
JakubGV <kubi.vogel7@hotmail.com>
2023-05-17 10:42:41 -0400

    Update icon and styling, slightly modify content

--- src/PersonalWebsite.tsx
+++ src/PersonalWebsite.tsx
@@ -13,11 +13,9 @@ import reactIcon from './media/ReactGroup.svg';
 import pythonIcon from './media/PythonGroup.svg';
 import sqlIcon from './media/SQLGroup.svg';
 
-
 import liIcon from './media/linkedin.png';
 import githubIcon from './media/GitHub-32px.png';
-import email from './media/email.svg';
-import phone from './media/phone.svg';
+
 import bee from './media/bee.svg';
 import squares from './media/squares.svg';
 
@@ -26,8 +24,8 @@ function PersonalWebsite() {
   const heroText = "I'm an aspiring software engineer. That means I like technology and writing code. But really, I have the opportunity to build and create solutions that can reach all types of people, all while learning new things every day.";
 
   
-  const about = ['Currently, I am a senior electrical and computer engineering student at Rutgers University as a member of the Honors College. On top of that, I am pursuing a minor in business administration to better integrate business and engineering principles to lead an engaging, challenging career.',
-                 'As for this website, I can proudly say I built it from scratch. There is nothing but handmade HTML, CSS, JavaScript, and a little help from ReactJS. If you have any questions about how I made it or otherwise, don\'t hesitate to contact me below!',
+  const about = ["I am a master's electrical and computer engineering student at Rutgers University from the Honors College. I have a minor in business administration and I am interested in better integrating business and engineering principles to lead an engaging, challenging career.",
+                 "As for this website, I can proudly say I built it from scratch. There is nothing but handmade HTML, CSS, JavaScript, and a little help from ReactJS. If you have any questions about how I made it or otherwise, don't hesitate to contact me below!",
                 ];
 
   const projects = [{
@@ -57,6 +55,8 @@ function PersonalWebsite() {
                 'Programmed 3 heat treat furnaces to collect key performance indicators and send them to the plant\'s database to give the Senior Controls Engineer more visibility into the process and allow for proactive maintenance.']
     }
   ];
+
+  const email = "jakub.vogel@rutgers.edu";
   
   return (
     <>
@@ -74,18 +74,18 @@ function PersonalWebsite() {
         </div>
 
         <div className="about-icons">
-          <img src={htmlIcon} width="125"/>
-          <img src={cssIcon} width="125"/>
-          <img src={jsIcon} width="125"/>
-          <img src={reactIcon} width="125"/>
-          <img src={pythonIcon} width="125"/>
-          <img src={sqlIcon} width="125"/>
+          <img src={htmlIcon} width="100" alt="HTML icon"/>
+          <img src={cssIcon} width="100" alt="CSS icon"/>
+          <img src={jsIcon} width="100" alt="JavaScript icon"/>
+          <img src={reactIcon} width="100" alt="React icon"/>
+          <img src={pythonIcon} width="100" alt="Python icon"/>
+          <img src={sqlIcon} width="100" alt="SQL icon"/>
         </div>
         <div className="about-text about">
           <h1>About Me</h1>
           {
-            about.map( (paragraph) => {
-              return <p>{paragraph}</p>
+            about.map( (paragraph, index) => {
+              return <p key={index}>{paragraph}</p>
             })
           }
         </div>
@@ -99,7 +99,7 @@ function PersonalWebsite() {
           <h1>Resume Highlights</h1>
           <ResumeCards highlights={resumeHighlights}/>
           <div className="link">
-            <a href={resumeLink} target="_blank" rel="noreferrer">
+            <a href={resumeLink} target="_blank" rel="noreferrer" title="resume link">
               <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 64 64"><g data-name="File Download"><path d="M42 52h-5V37a1 1 0 0 0-2 0v16a1 1 0 0 0 1 1h3.586L32 61.586 24.414 54H28a1 1 0 0 0 1-1V37a1 1 0 0 0-2 0v15h-5a1 1 0 0 0-.707 1.707l10 10a1 1 0 0 0 1.414 0l10-10A1 1 0 0 0 42 52zM36 34a1 1 0 0 0 1-1v-5a1 1 0 0 0-2 0v5a1 1 0 0 0 1 1zM33 33v-9a1 1 0 0 0-2 0v9a1 1 0 0 0 2 0zM28 34a1 1 0 0 0 1-1v-5a1 1 0 0 0-2 0v5a1 1 0 0 0 1 1z" fill="#28282b"/><path d="m50.707 9.293-9-9A1.091 1.091 0 0 0 41 0H14a1 1 0 0 0-1 1v46a1 1 0 0 0 1 1h10a1 1 0 0 0 0-2h-9V2h25v8a1 1 0 0 0 1 1h8v35h-9a1 1 0 0 0 0 2h10a1 1 0 0 0 1-1V10a1.092 1.092 0 0 0-.293-.707zM42 9V3.414L47.586 9z" fill="#28282b"/></g></svg>
             </a>
           </div>
@@ -112,7 +112,7 @@ function PersonalWebsite() {
 
         <footer className="footer">
           <p className="footer-text">Jakub Vogel</p>
-          <p className="footer-text">jakub.vogel@rutgers.edu</p>
+          <p className="footer-text">{email}</p>
           <p className="footer-text">(908) 210-6270</p>
           <div className="icons">
             <a href="https://www.linkedin.com/in/jakub-vogel" target="_blank" rel="noreferrer">
@@ -127,54 +127,6 @@ function PersonalWebsite() {
       </div>
       <div className="stripe"></div>
     </>
-    /*
-      <div className="content-grid">
-        <MenuBar header="Jakub Vogel" sections={sections}/>
-
-        <div className="pages">
-          <div className="home">
-            <div className="welcome-info">
-              <div className="left-home">
-                <img className="headshot" src={headshot} alt="Jakub Vogel headshot"/>
-              </div>
-              <div className="right-home">
-                <div className="description-name">Jakub Vogel</div>
-                <div className="caption caption-main">
-                  <span>I like to</span>
-                  <TypingText textOptions={rotatingWords}/>
-                </div>
-                <div className="caption">{homeText}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-     
-
-      <div id="contact">
-        <h2>Contact Me</h2>
-        <ContactForm />
-        <br />
-        <div className="line">Jakub Vogel</div>
-        <div className="line">
-          <img className="email-icon" src={email} width="20" height="20" alt="Email icon" />
-          jakub.vogel@rutgers.edu
-        </div>
-        <div className="line">
-          <img className="phone-icon" src={phone} width="20" height="20" alt="Phone icon" />
-          (908) 210-6270
-        </div>
-        <div className="icons">
-          <a href="https://www.linkedin.com/in/jakub-vogel" target="_blank" rel="noreferrer">
-            <img className="li-icon" src={liIcon} width="37.63" height="32" alt="Linked-in icon" />
-          </a>
-          <a href="https://github.com/JakubGV" target="_blank" rel="noreferrer">
-            <img className="github-icon" src={githubIcon} width="32" height="32" alt="GitHub icon" />
-          </a>
-        </div>
-      </div>
-    </>
-    */
   );
 }
 
